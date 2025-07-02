6f423a0feb753959b3c78806ad7d51fc2adb31b5
Civic Education Kenya <civiceducationkenya@gmail.com>
2025-07-02 21:38:49 +0300

    Update BlogPost.tsx

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -2,7 +2,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import Layout from '@/components/layout/Layout';
-import { blogService, BlogPost } from '@/services/blogService';
+import { blogService } from '@/services/blogService';
+import { BlogPost } from '@/services/blogService';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
 import { Card, CardContent } from '@/components/ui/card';
