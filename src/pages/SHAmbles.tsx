0d6f41d68c8935cf5d8f2a3972d7dc69fe518ca3
Civic Education Kenya <civiceducationkenya@gmail.com>
2025-08-28 16:23:20 +0300

    Update SHAmbles.tsx

--- src/pages/SHAmbles.tsx
+++ src/pages/SHAmbles.tsx
@@ -522,7 +522,7 @@ const SHAmbles: React.FC = () => {
 
   return (
     <>
-    <NavBar />
+    <Navbar />
     <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50/30 p-4 md:p-6">
       <div className="max-w-7xl mx-auto space-y-8">
         {/* Header */}
